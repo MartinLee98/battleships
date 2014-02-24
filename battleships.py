f42dbfda2454a1c1a8cd39c428e05d8e2087e6fa
andymitrich <andymitrich@gmail.com>
2014-02-24 20:29:57 +0400

    def user_place_ships

--- battleships.py
+++ battleships.py
@@ -12,9 +12,30 @@ for i in range(10):
 user_board = copy.deepcopy(board)
 comp_board = copy.deepcopy(board)
 ships = {
-	'aircraft carrier':5,
-	'battleship':4,
-	'submarine':3,
-	'destroyer':3,
-	'patrol boat':2
+	'aircraft carrier': 5,
+	'battleship': 4,
+	'submarine': 3,
+	'destroyer': 3,
+	'patrol boat': 2
 }
+
+def user_place_ships(board, ships):
+	for ship in ships.keys():
+		valid = False
+
+		while not valid:
+			print_board('u', board)
+			print("Placing a/an " + ship)
+			x,y = get_coords()
+			orientation = v_or_h()
+			valid = validate(board, ships[ship], x, y, orientation)
+
+			if not valid:
+				print("Cannot place a ship there.\nPlease take a look at the board and try again.")
+				raw_input("Hit ENTER to continue...")
+
+		board = place_ship(board, ships[ship], ship[0], x, y, orientation)
+		print_board('u', board)
+
+	raw_input("Done placing user ships. Hit ENTER to continue...")
+	return board
